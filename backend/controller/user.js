b111f2beb9a62fb61fa30afb9a810d69e3c2b24c
Abayraj <abayunni5@gmail.com>
2023-11-30 00:11:32 +0530

    signup user email found then submit image upload prevented

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -3,7 +3,7 @@ const path = require("path");
 const router = express.Router();
 const User = require("../model/User");
 const {upload} = require("../multer");
-
+const fs = require("fs");
 const ErrorHandler = require("../utils/ErrorHandler");
 
 
@@ -13,6 +13,17 @@ router.post("/create-user",upload.single("file"), async(req,res,next)=>{
     const userEmail = await User.findOne({email});
 
     if(userEmail){
+        const filename = req.file.filename;
+        const filePath = `uploads/${filename}`;
+        fs.unlink(filePath,(err)=>{
+            if(err){
+                console.log(err,"file errorr")
+                res.status(500).json({message:"Error deleting file "})
+            }
+            else{
+                res.json({message:"File deleted successfully"})
+            }
+        })
         return next(new ErrorHandler("User already exists",400));
     }
     const filename = req.file.filename;
@@ -21,9 +32,16 @@ router.post("/create-user",upload.single("file"), async(req,res,next)=>{
         name:name,
         email:email,
         password:password,
-        avatar:fileUrl,
+        avatar:{
+            url:fileUrl,
+            public_id:filename
+        },
     };
 
-    console.log(user)
-})
+    const newUser = await User.create(user);
+    res.status(201).json({
+        success:true,
+        newUser,
+    });
+});
 module.exports = router;
